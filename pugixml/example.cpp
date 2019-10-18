9cb9ae4e7353756533f72bd7bd8771c973650883
Your Name <you@example.com>
2019-10-17 19:49:56 -0500

    prueba xml

--- pugixml/example.cpp
+++ pugixml/example.cpp
@@ -9,11 +9,13 @@ int main()
     if (!result)
         return -1;
     //for (pugi::xml_node tool: doc.child("Profile").child("Tools").children("Tool"))
-    cout << doc.child("problem").attribute("nrWeeks").value();
-    for (pugi::xml_node tool: doc.child("problem").child("optimization").child("rooms"))
+   cout << doc.child("problem").child("rooms").child("room").attribute("capacity").value() <<endl;
+//    cout << doc.child("problem").attribute("nrWeeks").value();
+    for (pugi::xml_node tool: doc.child("problem").child("rooms"))
     {
-    cout << tool.attribute("id").value();
+    cout << tool.attribute("id").value() <<endl;
   //      if (timeout > 0)
   //          std::cout << "Tool " << tool.attribute("Filename").value() << " has timeout " << timeout << "\n";
     }
+    cout << doc.child("problem").child("rooms").child("room").find_child_by_attribute("id", "capacity", "55");
 }
